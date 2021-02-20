6ca9b33409569ae8225a9858ba5c540fc65b255c
SubasishMula2001 <74977100+SubasishMula2001@users.noreply.github.com>
2021-02-20 22:17:44 +0530

    Add files via upload

---
+++ SquarePatten.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class SquarePatten{
+public static void main(String []args){
+  Scanner s=new Scanner(System.in);
+    int i,j,n;
+    n=s.nextInt();
+    for(i=1;i<=n;i++){
+        
+        for(j=1;j<=n;j++){
+            System.out.print(n);
+         
+        }
+        System.out.println();
+        
+    }
+}
+}
